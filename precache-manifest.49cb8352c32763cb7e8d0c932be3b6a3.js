d615bc191a8a6e211885dd197328d695898c09dd
Tony <TonyLin3591@gmail.com>
2022-07-13 14:10:28 -0400

    Updates

--- precache-manifest.49cb8352c32763cb7e8d0c932be3b6a3.js
+++ precache-manifest.49cb8352c32763cb7e8d0c932be3b6a3.js
@@ -1,11 +1,11 @@
 self.__precacheManifest = [
   {
-    "revision": "02b00064fba50f087c92",
+    "revision": "677fccba750104eb59a0",
     "url": "/AirBnb/static/css/main.e0095bf8.chunk.css"
   },
   {
-    "revision": "02b00064fba50f087c92",
-    "url": "/AirBnb/static/js/main.1004556d.chunk.js"
+    "revision": "677fccba750104eb59a0",
+    "url": "/AirBnb/static/js/main.18dd0536.chunk.js"
   },
   {
     "revision": "c65a1c486b7fcbeec180",
@@ -28,7 +28,7 @@ self.__precacheManifest = [
     "url": "/AirBnb/static/media/logo.200ce038.png"
   },
   {
-    "revision": "04b374063ad52ffd343cdf7f0a339ff7",
+    "revision": "e6603b54a3cabed684174caa698e7d66",
     "url": "/AirBnb/index.html"
   }
 ];
