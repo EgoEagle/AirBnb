e752119517cb6258ce9b09e3fa2abf9135e09eb0
Tony <TonyLin3591@gmail.com>
2022-07-12 22:03:25 -0400

    Updates

---
+++ precache-manifest.a81aa883da43526fea6a1c7fc769dd88.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = [
+  {
+    "revision": "a27f0f602c7a0621332f",
+    "url": "/AirBnb/static/css/main.a8c366e8.chunk.css"
+  },
+  {
+    "revision": "a27f0f602c7a0621332f",
+    "url": "/AirBnb/static/js/main.c1a66c63.chunk.js"
+  },
+  {
+    "revision": "0e29ff48572d5bc8ee9d",
+    "url": "/AirBnb/static/js/runtime~main.e15a5dd8.js"
+  },
+  {
+    "revision": "0091620aaf765dc358ae",
+    "url": "/AirBnb/static/js/2.a843604c.chunk.js"
+  },
+  {
+    "revision": "1e38ecc495746bd18357",
+    "url": "/AirBnb/static/js/3.12bf67e3.chunk.js"
+  },
+  {
+    "revision": "06e733283fa43d1dd57738cfc409adbd",
+    "url": "/AirBnb/static/media/logo.06e73328.svg"
+  },
+  {
+    "revision": "8a8d83d88bba975f002c9774b6772963",
+    "url": "/AirBnb/static/media/grid.8a8d83d8.png"
+  },
+  {
+    "revision": "200ce038cd32f8cecb6e187f3280499c",
+    "url": "/AirBnb/static/media/logo.200ce038.png"
+  },
+  {
+    "revision": "2eec759331d5b891b9b5d9ee748f7e2d",
+    "url": "/AirBnb/index.html"
+  }
+];
---
+++ service-worker.js
@@ -0,0 +1,34 @@
+/**
+ * Welcome to your Workbox-powered service worker!
+ *
+ * You'll need to register this file in your web app and you should
+ * disable HTTP caching for this file too.
+ * See https://goo.gl/nhQhGp
+ *
+ * The rest of the code is auto-generated. Please don't update this file
+ * directly; instead, make changes to your Workbox build configuration
+ * and re-run your build process.
+ * See https://goo.gl/2aRDsh
+ */
+
+importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
+
+importScripts(
+  "/AirBnb/precache-manifest.a81aa883da43526fea6a1c7fc769dd88.js"
+);
+
+workbox.clientsClaim();
+
+/**
+ * The workboxSW.precacheAndRoute() method efficiently caches and responds to
+ * requests for URLs in the manifest.
+ * See https://goo.gl/S9QRab
+ */
+self.__precacheManifest = [].concat(self.__precacheManifest || []);
+workbox.precaching.suppressWarnings();
+workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
+
+workbox.routing.registerNavigationRoute("/AirBnb/index.html", {
+  
+  blacklist: [/^\/_/,/\/[^/]+\.[^/]+$/],
+});
